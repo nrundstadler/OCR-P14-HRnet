42b8a522ba7d741dd67ce54ab9ff36e38c67af6e
Nicolas Rundstadler <n.rundstadler@laposte.net>
2025-06-05 17:03:15 +0200

    fix: change class to className in Background component for React JSX compliance

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Background = () => {
   return (
     // prettier-ignore
-    <div class="
+    <div className="
     absolute top-0 left-1/2 -z-10 ml-[-19em] h-[25rem] w-[35rem]
     bg-gradient-to-r from-[#5a7002] to-[#93ac17]
     [mask-image:radial-gradient(farthest-side_at_top,white,transparent)]
